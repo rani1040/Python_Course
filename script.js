5d918ac392e5a5d408c556ade390cfab598ba6ef
Rani Rathore <2020btechairani8719@poornima.edu.in>
2024-11-02 16:33:21 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+function toggleText() {
+    const extraContent = document.getElementById("extraContent");
+    if (extraContent.style.display === "none") {
+        extraContent.style.display = "block";
+    } else {
+        extraContent.style.display = "none";
+    }
+}
